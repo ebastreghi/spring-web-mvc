e9dc22c050dba7043965531325f4e6ee497a7235
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-01-21 08:10:56 -0300

    Rest
    get, post, put

---
+++ src/main/java/br/com/globallabs/springwebmvc/rest/JediResource.java
@@ -0,0 +1,53 @@
+package br.com.globallabs.springwebmvc.rest;
+
+import br.com.globallabs.springwebmvc.model.Jedi;
+import br.com.globallabs.springwebmvc.repository.JediRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class JediResource {
+
+    @Autowired
+    private JediRepository repository;
+
+    @GetMapping("/api/jedi")
+    public List<Jedi> getAllJedi(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/api/jedi/{id}")
+    public ResponseEntity<Jedi> getJedi(@PathVariable("id") Long id){
+        final Optional<Jedi> jedi = repository.findById(id);
+        if(jedi.isPresent()){
+            return ResponseEntity.ok(jedi.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/api/jedi")
+    public Jedi createJedi(@Valid @RequestBody Jedi jedi){
+        return repository.save(jedi);
+    }
+
+    @PutMapping("/api/jedi/{id}")
+    public ResponseEntity<Jedi> updateJedi(@PathVariable("id") Long id, @Valid @RequestBody Jedi dto){
+        final Optional<Jedi> jediEntity = repository.findById(id);
+        final Jedi jedi;
+        if(jediEntity.isPresent()){
+            jedi = jediEntity.get();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+        jedi.setName(dto.getName());
+        jedi.setLastName(dto.getLastName());
+        return ResponseEntity.ok(repository.save(jedi));
+    }
+
+}
